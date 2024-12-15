6237a0810845a165a03b66ffc6f6d608bbc0d937
Nick Yoon <89yunik@gmail.com>
2024-12-15 15:39:35 +0900

    setting : fix log path and level

--- logging_config.py
+++ logging_config.py
@@ -1,13 +1,19 @@
 import logging
+from pathlib import Path
 from logging.handlers import RotatingFileHandler
 
 def setup_logging():
+    current_file_path = Path(__file__).resolve()
+    log_dir = current_file_path.parent / 'logs'
+    log_dir.mkdir(parents=True, exist_ok=True)
+    log_file_path = log_dir / 'app.log' 
+
     log_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-    log_handler = RotatingFileHandler('app.log', maxBytes=5*1024*1024, backupCount=3)
+    log_handler = RotatingFileHandler(log_file_path, maxBytes=5*1024*1024, backupCount=3)
     log_handler.setFormatter(log_formatter)
     
     logger = logging.getLogger()
-    logger.setLevel(logging.ERROR)
+    logger.setLevel(logging.INFO)
     logger.addHandler(log_handler)
     
     return logger
