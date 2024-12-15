90e80e272ce48cf75bd4f07bc93fd4dd0fe60f10
Nick Yoon <89yunik@gmail.com>
2024-12-15 15:58:25 +0900

    setting : fix handle_exceptions logging format_exc & exit_program

--- decorators.py
+++ decorators.py
@@ -1,10 +1,13 @@
-def handle_exceptions(logger):
+from traceback import format_exc
+from sys import exit
+
+def handle_exceptions(logger, exit_program=True):
     def decorator(func):
         def wrapper(*args, **kwargs):
             try:
                 return func(*args, **kwargs)
             except Exception as e:
-                logger.error(f"Exception occurred in {func.__name__}: {e}")
-                return None
+                logger.error(f"Exception occurred in {func.__name__}: {e}\n{format_exc()}")
+                if exit_program: exit(1)
         return wrapper
     return decorator
